a8b4d36aae91efbd95de3993f11aff13c1f44039
Hatsu990 <Hatsu990@users.noreply.github.com>
2026-05-26 01:06:06 +0900

    Update cafe address

--- lib/constants/site.ts
+++ lib/constants/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "홍천 꽃신",
   subtitle: "꽃피는 신장대리, 마을 카페",
   phone: "033-000-0000",
-  address: "강원특별자치도 홍천군 (예시 주소)",
+  address: "강원특별자치도 홍천군 홍천읍 홍천로5길 10 1층",
   kakaoChannel: "@hongcheon-kkotsin",
   businessHours: "매일 10:00 - 20:00",
 };
